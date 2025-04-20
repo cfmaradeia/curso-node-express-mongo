a6d06b3d4fb8336660d6a7f20ee7d9432ed7f3eb
cfmaradeia <cfmaradeia@gmail.com>
2025-04-20 20:58:19 -0300

    Add rotas

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import http from "http";
 
 const PORT = 3000;
+
+const rotas = {
+    "/": "Curso do Express API",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"content-type":"text/plain"});
-    res.end(
-        
-    );
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
