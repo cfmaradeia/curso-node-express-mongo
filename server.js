59b9cfef7fd57c89ce51138195375093c266c8df
cfmaradeia <cfmaradeia@gmail.com>
2025-04-20 20:52:22 -0300

    Add arquivos

---
+++ server.js
@@ -0,0 +1,14 @@
+import http from "http";
+
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"content-type":"text/plain"});
+    res.end(
+        
+    );
+});
+
+server.listen(PORT, () => {
+    console.log("servidor em execução");
+});
+ 
