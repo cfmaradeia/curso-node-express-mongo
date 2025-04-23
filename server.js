2b855dffb9aac089fd911093e8ec9eb11b8cad95
cfmaradeia <cfmaradeia@gmail.com>
2025-04-23 19:36:01 -0300

    Rotas de crud

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-import http from "http";
+import app from "./src/app.js"
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso do Express API",
-    "/livros": "Entrei na rota livros",
-    "/autores": "Entrei na rota autores"
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"content-type":"text/plain"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("servidor em execução");
 });
  
